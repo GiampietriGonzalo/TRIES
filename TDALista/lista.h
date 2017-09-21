20f864a3db55e47bc83f6c9bae1ae2bafa6efdc1
Gonza <Giampietri.Gonzalo@protonmail.com>
2017-09-21 00:34:04 -0300

    Pruebo giladas

---
+++ TDALista/lista.h
@@ -0,0 +1,48 @@
+#define lista_h
+
+#ifndef LISTA_INCLUDED
+#define LISTA_INCLUDED
+
+
+
+
+typedef struct celda * TPosicion;
+typedef void * TElemento;
+
+typedef struct celda {
+    TElemento elemento;
+    struct celda* proxima_celda;
+} * TCelda;
+
+typedef struct lista {
+    unsigned int cantidad_elementos;
+    TCelda primer_celda;
+} * TLista;
+
+
+TLista crear_lista();
+int l_insertar(TLista lista, TPosicion pos, TElemento elem);
+
+TLista crear_lista();
+
+int l_insertar(TLista lista, TPosicion pos, TElemento elem);
+
+int l_eliminar(TLista lista, TPosicion pos);
+
+TPosicion l_primera(TLista lista);
+
+TPosicion l_ultima(TLista lista);
+
+int l_size(TLista lista);
+
+TPosicion l_anterior(TLista lista, TPosicion pos);
+
+TPosicion l_siguiente(TLista lista,TPosicion pos);
+
+TElemento l_recuperar(TLista lista, TPosicion pos);
+
+
+
+
+
+#endif // lista_h
